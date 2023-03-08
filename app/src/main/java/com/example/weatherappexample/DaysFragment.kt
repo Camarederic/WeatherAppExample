61eb32631e7a225a4b20090c4fec4fddb7e9fae7
Camarederic <camarederic@mail.ru>
2023-03-08 18:14:03 +0300

    Set the temperature in DaysFragment

--- app/src/main/java/com/example/weatherappexample/DaysFragment.kt
+++ app/src/main/java/com/example/weatherappexample/DaysFragment.kt
@@ -5,17 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherappexample.adapters.WeatherAdapter
+import com.example.weatherappexample.databinding.FragmentDaysBinding
 
 
 class DaysFragment : Fragment() {
 
+    private lateinit var binding: FragmentDaysBinding
+    private lateinit var adapter: WeatherAdapter
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_days, container, false)
+        binding = FragmentDaysBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+
+        model.liveDataList.observe(viewLifecycleOwner) {
+            adapter.submitList(it.subList(1, it.size))
+        }
+    }
+
+    private fun initRecyclerView() = with(binding) {
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        recyclerView.adapter = adapter
     }
 
     companion object {
